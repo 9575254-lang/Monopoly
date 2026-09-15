6bb241eb5a8fc7cf1bc42a1071b6a331fec39789
Skye Hines <9575254@philasd.org>
2026-09-15 17:35:37 +0000

    first commit

---
+++ Vexcode_monopoly.py
@@ -0,0 +1,36 @@
+FWD = Event()
+LT = Event()
+RT = Event()
+myVariable = 0
+current_space_number = 0
+dice1 = 0
+dice2 = 0
+spaces_to_move = 0
+
+def cpmplete_task():
+    global my_event, FWD, LT, RT, myVariable, current_space_number, dice1, dice2, spaces_to_move
+    pass
+
+def when_started1():
+    global my_event, FWD, LT, RT, myVariable, current_space_number, dice1, dice2, spaces_to_move
+    pass
+
+def FWD_callback_0():
+    global my_event, FWD, LT, RT, myVariable, current_space_number, dice1, dice2, spaces_to_move
+    pass
+
+def FWD_callback_1():
+    global my_event, FWD, LT, RT, myVariable, current_space_number, dice1, dice2, spaces_to_move
+    pass
+
+def LT_callback_0():
+    global my_event, FWD, LT, RT, myVariable, current_space_number, dice1, dice2, spaces_to_move
+    pass
+
+def LT_callback_1():
+    global my_event, FWD, LT, RT, myVariable, current_space_number, dice1, dice2, spaces_to_move
+    pass
+
+def RT_callback_0():
+    global my_event, FWD, LT, RT, myVariable, current_space_number, dice1, dice2, spaces_to_move
+    pass
